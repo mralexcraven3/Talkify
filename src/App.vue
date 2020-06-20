01472aa406ca6890674960c56b5a4a33fb9b7fd0
Alex Craven <12472145+mralexcraven3@users.noreply.github.com>
2020-06-20 20:22:59 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  width: 100vh;
+  height: 100vh;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
