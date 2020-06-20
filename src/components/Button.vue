a35ed0f8f27fee9195cada323c1b1f635ffadbca
Alex Craven <12472145+mralexcraven3@users.noreply.github.com>
2020-06-20 20:24:08 +0100

    Add files via upload

---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.button {
+  background: #00c1f9;
+  color: white;
+  padding: 0.5rem 2rem;
+  font-size: 1.5rem;
+  outline: none;
+  border: none;
+  border-radius: 100px;
+  margin: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+
+.button:active {
+  box-shadow: 0.1em 0.1em 0.2em rgba(0, 193, 249, 1);
+}
+</style>
